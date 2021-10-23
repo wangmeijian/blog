e16d48cfb2de8ed9077bbc36ee73a04cd167c9c0
wangmeijian <>
2021-10-23 22:33:11 +0800

    利用vuepress搭建静态blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,13 +2,15 @@ module.exports = {
   title: 'Undefined的笔记本',
   description: 'Undefined的笔记本',
   base: '/blog/',
+  displayAllHeaders: true,
   themeConfig: {
-    sidebarDepth: 2,
     nav: [
+      { text: '首页', link: '/' },
+      { text: '大杂烩', link: '/comprehensive/' },
       { text: 'GitHub', link: 'https://github.com/wangmeijian' },
     ],
-    sidebar: [
-      '/guide/',
-    ]
   },
+  plugins: [
+    ["vuepress-plugin-auto-sidebar", {}]
+  ]
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,16 @@
-# docs
+---
+home: true
+lang: zh
+heroText: Undefined的笔记本
+tagline: 一个非著名前端开发者的博客
+actionText: 随便看看 →
+actionLink: /comprehensive/
+features:
+- title: 记录
+  details: 日常开发和学习，经常会遇到各种问题，记录下来
+- title: 分享
+  details: 踩过的坑，积累的经验，分享出去，才更有价值
+- title: 备忘
+  details: 若干年后，回过头来看，哦，原来我还学过这个
+footer: MIT Licensed | Copyright © 2021-present Undefined
+---
