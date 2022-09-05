e98c7b4649909fd45d82c61da47492ab6b925c1e
王美建 <>
2022-09-05 18:06:01 +0800

    更新README

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-# Wang Blog
+# 老王说
 
-[随便看看 →](https://wangmeijian.github.io/blog/)
+[查看全部文章 →](https://wangmeijian.github.io/blog/)
+
+
+[深入了解Chrome插件](https://github.com/wangmeijian/blog/issues/12)
+
+[pnpm凭什么这么快](https://github.com/wangmeijian/blog/issues/11)
+
+[透过inode来理解硬链接和软链接](https://github.com/wangmeijian/blog/issues/10)
+
+[纯HTML+CSS 拉窗帘效果](https://github.com/wangmeijian/blog/issues/7)
+
+[如何快速绘制任意角度的扇形？](https://github.com/wangmeijian/blog/issues/6)
+
+[session、token和cookie](https://github.com/wangmeijian/blog/issues/5)
+
+[用docker搭建前端环境](https://github.com/wangmeijian/blog/issues/4)
+
+[手写系列：call、apply、bind、函数柯里化](https://github.com/wangmeijian/blog/issues/3)
+
+[Promise是如何实现异步编程的？](https://github.com/wangmeijian/blog/issues/2)
+
+[React中共享组件逻辑的三种方式](https://github.com/wangmeijian/blog/issues/1)
+
+
 
 > 部署：```sh deploy.sh```
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,16 @@
 module.exports = {
-  title: 'Wang Blog',
-  description: 'Wang Blog前端技术博客，session、token和cookie，React中共享组件逻辑的三种方式，Event loops秒懂，Vue.nexttick，Webpack,web系统中的权限控制,深入webpack热更新,Vue的数据异步更新机制Vue.nextTick',
-  base: '/blog/',
+  title: "老王说",
+  description:
+    "老王说前端技术博客，session、token和cookie，React中共享组件逻辑的三种方式，Event loops秒懂，Vue.nexttick，Webpack,web系统中的权限控制,深入webpack热更新,Vue的数据异步更新机制Vue.nextTick",
+  base: "/blog/",
   displayAllHeaders: true,
   themeConfig: {
-    lastUpdated: '上次更新',
+    lastUpdated: "上次更新",
     nav: [
-      { text: '首页', link: '/' },
-      { text: '大杂烩', link: '/comprehensive/' },
-      { text: 'GitHub', link: 'https://github.com/wangmeijian' },
+      { text: "首页", link: "/" },
+      { text: "大杂烩", link: "/comprehensive/" },
+      { text: "GitHub", link: "https://github.com/wangmeijian" },
     ],
   },
-  plugins: [
-    ["vuepress-plugin-auto-sidebar", {}]
-  ]
-}
+  plugins: [["vuepress-plugin-auto-sidebar", {}]],
+};
